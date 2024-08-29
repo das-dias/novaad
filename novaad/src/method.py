419d60aad1e0e3ab0b167c382572a8b1a99e7e6c
dasdias <das.dias6@gmail.com>
2024-08-29 10:29:43 +0200

    Initial commit.

---
+++ novaad/src/method.py
@@ -0,0 +1,27 @@
+def input_referred_flicker_noise_psd(): pass
+
+def input_referred_thermal_noise_psd(): pass
+
+def interpolate(bot_row, top_row):pass
+
+class Method(object):
+  def __init__(self) -> None:
+    super().__init__()
+  
+  def look_up(self):
+    pass
+
+class Gmid(Method):
+  def __init__(self) -> None:
+    super().__init__()
+  
+  def look_up(self):
+    pass
+
+class Astar(Method):
+  def __init__(self) -> None:
+    super().__init__()
+  
+  def look_up(self):
+    pass
+  
